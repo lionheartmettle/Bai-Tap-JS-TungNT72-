1014bb638940caf5ae0a6153b5e896c9aa244f06
lionheartmettle <43745212+lionheartmettle@users.noreply.github.com>
2019-01-22 12:31:07 +0700

    qqq

--- TungNT72-21-01/arraySearch.js
+++ TungNT72-21-01/arraySearch.js
@@ -1,8 +1,43 @@
-function arrSeach(arr,num) {
-    for (var i = 0; i < arr.length; i += 1) {
-        if (num <= arr[i]) {
-            return i;
+// function arrSeach(arr,num) {
+//     for (var i = 0; i < arr.length; i += 1) {
+//         if (num <= arr[i]) {
+//             return i;
+//         }
+//     }
+//     return i;
+// }
+
+function arrSeach(arr, num) {
+    let star = 0;
+    let end = arr.length - 1;
+    let m = Math.floor(end / 2);
+    
+    function dequy(st, en) {
+      m = Math.floor((st + en) / 2);
+      if (num == arr[m]) {
+        return m;
+      }
+      if (st == arr.length - 2) {
+        if (num == arr[m + 1]) {
+          return m + 1;
         }
+        return 'Not found ! and it would be :' + (m + 2);
+      }
+      if (en == 1) {
+        return 'Not found ! and it would be :' + 0;
+      }
+      if (m == st) {
+        return 'Not found ! and it would be :' + (m + 1);
+      }
+      if (num < arr[m]) {
+        en = m;
+        return dequy(st, en);
+      }
+      if (num > arr[m]) {
+        st = m;
+        return dequy(st, en);
+      }
     }
-    return i;
-}
+    return dequy(star, end);
+  }
+  arrSeach([1, 2, 3, 4, 5, 7, 8, 9], -1);
--- TungNT72-21-01/binaryTreeSearch.js
+++ TungNT72-21-01/binaryTreeSearch.js
@@ -1,35 +1,42 @@
-function search(arr, num) {  
-    let star = 0;
-    let end = arr.length - 1;
-    let m = Math.floor(end / 2);
-    // while (num != arr[m] && star != (arr.length - 2)) {
-    //   if (num < arr[m]) {
-    //     end = m;
-    //   }
-    //   else {
-    //     star = m;
-    //   }
-    //   m = Math.floor((star + end) / 2);
-    // }
-    function dequy(st,en) {    
-      m = Math.floor((st + en) / 2);
-      if (num == arr[m]) {
-        return m;
+function search(arr, num) {
+  let star = 0;
+  let end = arr.length - 1;
+  let m = Math.floor(end / 2);
+  // while (num != arr[m] && star != (arr.length - 2)) {
+  //   if (num < arr[m]) {
+  //     end = m;
+  //   }
+  //   else {
+  //     star = m;
+  //   }
+  //   m = Math.floor((star + end) / 2);
+  // }
+  function dequy(st, en) {
+    m = Math.floor((st + en) / 2);
+    if (num == arr[m]) {
+      return m;
+    }
+    if (st == arr.length - 2) {
+      if (num == arr[m + 1]) {
+        return m + 1;
       }
-      if (st == arr.length - 2) {
-        if (num == arr[m+1]) {
-          return m+1;
-        }
-        return 'Not found !';
-      }
-      if (num < arr[m]) {
-        en = m;
-        return dequy(st,en);
-      }
-      if (num > arr[m]) {
-        st = m;
-        return dequy(st,en);
-      }
-    }  
-    return dequy(star,end);
+      return 'Not found ! and it would be :' + (m + 2);
+    }
+    if (en == 1) {
+      return 'Not found ! and it would be :' + 0;
+    }
+    if (m == st) {
+      return 'Not found ! and it would be :' + (m + 1);
+    }
+    if (num < arr[m]) {
+      en = m;
+      return dequy(st, en);
+    }
+    if (num > arr[m]) {
+      st = m;
+      return dequy(st, en);
+    }
   }
+  return dequy(star, end);
+}
+search([1, 2, 3, 4, 5, 7, 8, 9], -1);
